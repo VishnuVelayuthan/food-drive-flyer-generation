76bc5627fb4d7c192715f7647a9c07d3034073f3
Vishnu Velayuthan <100023569@mvla.net>
2021-11-07 12:20:54 -0800

    got the fonts stuff to work and did a little bit of javadocs

--- GraphicDesigner.py
+++ GraphicDesigner.py
@@ -5,9 +5,22 @@ import qrcode
 
 Template_Path = Path("designs", "template.png")
 
-
+Fonts = {
+    "sub-header": ImageFont.truetype("Roboto-Regular.ttf", size=41.1),
+    "link": ImageFont.truetype("Roboto-Regular.ttf", size=29.2)
+}
 
 def make_flyer(teacher_name, room_number, donation_link):
+    """
+    creates flyer from template
+
+    @param teacher_name: name of teacher
+    @param room_number: room number of teacher
+    @param donation_link: link to donation link
+
+    @return img object of flyer
+    """
+
     flyer_img = Image.open(Template_Path)
 
     #qr_img = qr_code_generator(donation_link)
