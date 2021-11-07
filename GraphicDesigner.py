3a4f4fd2d6bc932b8c41fba7016db01531164d28
Vishnu Velayuthan <100023569@mvla.net>
2021-11-07 13:05:16 -0800

    Adjusted the stuff for center alignment of subheader found out subheader adjustment pixel count

--- GraphicDesigner.py
+++ GraphicDesigner.py
@@ -3,10 +3,10 @@ from pathlib import Path
 import textwrap
 import qrcode 
 
-Template_Path = Path("designs", "template.png")
+Template_Path = Path("designs", "template-trial.png")
 
 Fonts = {
-    "sub-header": ImageFont.truetype("Roboto-Regular.ttf", size=63),
+    "sub-header": ImageFont.truetype("Roboto-Regular.ttf", size=55),
     "link": ImageFont.truetype("Roboto-Regular.ttf", size=29)
 }
 
@@ -22,12 +22,17 @@ def make_flyer(teacher_name, room_number, donation_link):
     """
 
     flyer_img = Image.open(Template_Path)
+    W, H = (flyer_img.width, flyer_img.height)
 
     flyer_editor = ImageDraw.Draw(flyer_img)
 
+    
     sub_header = teacher_name + " - " + room_number
-
-    flyer_editor.text((550,500),sub_header, font=Fonts["sub-header"])
+    sub_height_adj = 610 # trial and error
+    # trying to get sub-header aligned with image
+    w_sub, h_sub = flyer_editor.textsize(sub_header, font=Fonts["sub-header"])
+    flyer_editor.text(((W-w_sub)/2, (H-h_sub-sub_height_adj)/2), \
+        sub_header, font=Fonts["sub-header"])
 
     flyer_img.save("gggggg.png", "PNG")
     #qr_img = qr_code_generator(donation_link)
@@ -45,5 +50,5 @@ def qr_code_generator(donation_link):
 
 #main for debugging and testing
 if __name__ == "__main__":
-    make_flyer("fgds", "sfdgsdfg", "",)
+    make_flyer("Teacher Name", "Room #", "",)
     
