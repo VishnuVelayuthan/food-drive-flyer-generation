b79d38c02c3690d76ef1e70b72f56d522fe0d60b
Vishnu Velayuthan <100023569@mvla.net>
2021-11-07 12:43:30 -0800

    Starting to do positioning: edited the template to ake it a little bit better trying to figure out font size and positioning

--- GraphicDesigner.py
+++ GraphicDesigner.py
@@ -6,8 +6,8 @@ import qrcode
 Template_Path = Path("designs", "template.png")
 
 Fonts = {
-    "sub-header": ImageFont.truetype("Roboto-Regular.ttf", size=41.1),
-    "link": ImageFont.truetype("Roboto-Regular.ttf", size=29.2)
+    "sub-header": ImageFont.truetype("Roboto-Regular.ttf", size=63),
+    "link": ImageFont.truetype("Roboto-Regular.ttf", size=29)
 }
 
 def make_flyer(teacher_name, room_number, donation_link):
@@ -23,12 +23,27 @@ def make_flyer(teacher_name, room_number, donation_link):
 
     flyer_img = Image.open(Template_Path)
 
+    flyer_editor = ImageDraw.Draw(flyer_img)
+
+    sub_header = teacher_name + " - " + room_number
+
+    flyer_editor.text((550,500),sub_header, font=Fonts["sub-header"])
+
+    flyer_img.save("gggggg.png", "PNG")
     #qr_img = qr_code_generator(donation_link)
 
 
 def qr_code_generator(donation_link):
+    """
+    Makes qr code 
+
+    @param donation_link: link to donation link 
+
+    @return img object of qr code
+    """
     return qrcode.make(donation_link)
 
+#main for debugging and testing
 if __name__ == "__main__":
-    make_flyer("", "", "",)
+    make_flyer("fgds", "sfdgsdfg", "",)
     
